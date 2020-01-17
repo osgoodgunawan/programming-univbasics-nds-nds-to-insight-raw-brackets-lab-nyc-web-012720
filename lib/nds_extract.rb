23d773240fd58810f0a253f36156b040d5827b26
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-17 01:33:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,12 +9,12 @@ def directors_totals(nds)
   
   result={}
   outside_index=0
-  
+  #total=0
   #if you put total=0 outside the while loop then it wont go through the first while loop , total just going to add up and not become zero again for the next index
-   total=0
+   
   while outside_index<nds.length do 
   name=nds[outside_index][:name]  
- 
+  total=0
   movies_index=0
   movies_list=nds[outside_index][:movies]
   
